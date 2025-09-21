d1df1f57f573045ed3b714f9bf2cfc7ed89600f7
Алибек <iskakov.a@phystech.edu>
2025-09-22 02:34:16 +0600

    Update 11.cpp

--- seminar2_function/11.cpp
+++ seminar2_function/11.cpp
@@ -5,7 +5,7 @@ void multiply(float a[MAX][MAX], float b[MAX][MAX], float c[MAX][MAX], int n)
 {
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
-            c[i][j] = 0; // явное обнуление
+            c[i][j] = 0; 
             for (int k = 0; k < n; k++) {
                 c[i][j] += a[i][k] * b[k][j];
             }

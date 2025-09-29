fe9a58616ffd2141ab2ef4cbad47b8ca48a7accd
Алибек <iskakov.a@phystech.edu>
2025-09-30 01:28:45 +0600

    Create 12.cpp

---
+++ seminar3_string/12.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        printf("Использование: %s <слово> <число>\n", argv[0]);
+        return 1;
+    }
+    
+    char *word = argv[1];
+    int count = atoi(argv[2]);
+    
+    for (int i = 0; i < count; i++) {
+        printf("%s", word);
+        if (i < count - 1) {
+            printf(" "); 
+        }
+    }
+    printf("\n");
+    
+    return 0;
+}

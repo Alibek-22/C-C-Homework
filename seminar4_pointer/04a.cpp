c3570184b0643a7279df6edf3df980536d64beac
Алибек <iskakov.a@phystech.edu>
2025-10-07 01:35:07 +0600

    Create 04a.cpp

---
+++ seminar4_pointer/04a.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void mult2(int* p, size_t n) {
+    for (size_t i = 0; i < n; i++) {
+        *(p + i) *= 2;
+    }
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5};
+    size_t n = sizeof(arr) / sizeof(arr[0]);
+
+    mult2(arr, n);
+
+    for (size_t i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}

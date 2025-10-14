346ab51230db654ab272132907257cf5530ec188
Алибек <iskakov.a@phystech.edu>
2025-10-15 02:13:13 +0600

    Create 07.cpp

---
+++ seminar5_segments/07.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <math.h>
+
+size_t count_if(int* a, size_t n, int (*predicate)(int))
+{
+    size_t count = 0;
+    for (size_t i = 0; i < n; ++i)
+    {
+        if (predicate(a[i]))
+            ++count;
+    }
+    return count;
+}
+
+int is_negative(int x)
+{
+    return x < 0;
+}
+
+int is_even(int x)
+{
+    return x % 2 == 0;
+}
+
+int is_square(int x)
+{
+    if (x < 0)
+        return 0;
+    int root = (int)(sqrt(x) + 0.5); // округляем
+    return root * root == x;
+}
+
+int main()
+{
+    int a[] = {89, 81, 28, 52, 44, 16, -64, 49, 52, -79};
+
+    printf("%zu\n", count_if(a, 10, is_negative)); // Должен напечатать 2
+    printf("%zu\n", count_if(a, 10, is_even));     // Должен напечатать 6
+    printf("%zu\n", count_if(a, 10, is_square));   // Должен напечатать 3
+
+    return 0;
+}
